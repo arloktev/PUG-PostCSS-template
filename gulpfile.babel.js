51c832754abae1d20d14c4ff80a35187d1ca98e8
arloktev <arloktev@mail.ru>
2019-08-05 12:54:24 +0300

    Подкорректировал сборку

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -16,13 +16,34 @@ const paths = {
       './src/pug/**/*.pug'
     ]
   },
+  styles: {
+    src: './src/assets/css/main.css',
+    dist: './dist/assets/css/',
+    watch: [
+        './src/blocks/**/*.css',
+        './src/assets/css/**/*.css'
+    ]
+  },
   scripts: {
     src: './src/assets/js/index.js',
     dist: './dist/assets/js/',
     watch: [
-        './src/blocks/**/*.js',
-        './src/assets/js/**/*.js'
+      './src/blocks/**/*.js',
+      './src/assets/js/**/*.js'
     ]
+  },
+  images: {
+    src: [
+      './src/assets/img/**/*.{jpg,jpeg,png,gif,tiff,svg}',
+      '!./src/assets/img/favicon/*.{jpg,jpeg,png,gif,tiff}'
+    ],
+    dist: './dist/assets/img/',
+    watch: './src/assets/img/**/*.{jpg,jpeg,png,gif,svg}'
+  },
+  fonts: {
+    src: './src/assets/fonts/**/*.{ttf,woff,woff2}',
+    dist: './dist/assets/fonts/',
+    watch: './src/assets/fonts/**/*.{ttf,woff,woff2}'
   },
 };
 
@@ -31,12 +52,12 @@ requireDir('./tasks/');
 export { paths }
 
 export const development = gulp.series('clean',
-  gulp.parallel(['pages', 'scripts']),
+  gulp.parallel(['pages', 'styles', 'scripts', 'images', 'fonts']),
   gulp.parallel('server')
 );
 
 export const production = gulp.series('clean',
-  gulp.series(['pages', 'scripts'])
+  gulp.series(['pages', 'styles', 'scripts', 'images', 'fonts'])
 );
 
 export default development;
